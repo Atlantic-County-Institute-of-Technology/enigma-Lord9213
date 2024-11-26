60753c97fb1b8b5d26454b4f8058f889777f978c
Lord9213 <Lord9213@student.acitech.org>
2024-11-26 10:40:05 -0500

    Enigma Machine Project Final Submission

--- enigma_template.py
+++ enigma_template.py
@@ -16,16 +16,19 @@ def encode_message():
     for x in text.lower():
         if x not in alphabet:
             EOut += x
+    # for each character in text it checks if its not in alphabet, if it isn't it just re-adds the character to EOut
         else:
             if (alphabet.find(x) + key) > len(alphabet):
-                g = (int(alphabet.find(x)) - 26) + key
+                g = (int(alphabet.find(x)) + 26) + key
                 l = alphabet[g]
                 EOut += l
+    # for each character in text it checks if the math would go over the length of the alphabet, if it does it does more math and finds the correct character in the alphabet to change it to based on the key then adds it to EOut
             else:
                 o = alphabet.find(x) + key
                 k = alphabet[o]
                 EOut += k
     print(EOut)
+    # If the math does not exceed the length of the alphabet then it just finds the correct character to change it to and adds it to EOut without doing that more math
 # encodes a target file, similarly to encode_message, except now targeting a filename
 def encode_file():
     EOut = ""
@@ -33,29 +36,78 @@ def encode_file():
     with open(FI) as file:
         f = file.read()
     key = int(input("whats the key that you want to use for the cypher?"))
+    # opens and reads the file that the user wants to encode, aswell as asking what number they want the key to be
     for x in f.lower():
         if x not in alphabet:
             EOut += x
+    # for each character in the file it checks if its not in alphabet, if it isn't it just re-adds the character to EOut
         else:
             if (alphabet.find(x) + key) > len(alphabet):
                 g = (int(alphabet.find(x)) - 26) + key
                 l = alphabet[g]
                 EOut += l
+        # for each character in text it checks if the math would go over the length of the alphabet, if it does it does more math and finds the correct character in the alphabet to change it to based on the key then adds it to EOut
             else:
                 o = alphabet.find(x) + key
                 k = alphabet[o]
                 EOut += k
     print(EOut)
     file.close()
+    # If the math does not exceed the length of the alphabet then it just finds the correct character to change it to and adds it to EOut without doing that more math, and then closes the file
+
 # decodes target file using a user-specified key. If key is unknown, a keypress should
 # call decode_unknown_key()
 def decode_file():
-    pass
+    EOut = ""
+    FI = input("what file do you want to decode?")
+    with open(FI) as file:
+        f = file.read()
+    key = int(input("whats the key that you want to use for the cypher?"))
+    # opens and reads the file that the user wants to decode, aswell as asking what number they want the key to be
+    for x in f.lower():
+        if x not in alphabet:
+            EOut += x
+    # for each character in the file it checks if its not in alphabet, if it isn't it just re-adds the character to EOut
+        else:
+            if (alphabet.find(x) - key) > len(alphabet):
+                g = (int(alphabet.find(x)) + 26) - key
+                l = alphabet[g]
+                EOut += l
+            # for each character in text it checks if the math would go over the length of the alphabet, if it does it does more math and finds the correct character in the alphabet to change it to based on the key then adds it to EOut
+            else:
+                o = alphabet.find(x) - key
+                k = alphabet[o]
+                EOut += k
+    print(EOut)
+    file.close()
+    # If the math does not exceed the length of the alphabet then it just finds the correct character to change it to and adds it to EOut without doing that more math, and then closes the file
 
 # runs if the key is unknown. If this is true, print out all possible decoding combinations.
-def decode_unknown_key(filename):
-   pass
-
+def decode_unknown_key():
+    FI = input("what file do you want to decode?")
+    with open(FI) as file:
+        f = file.read()
+    # opens and reads the file that the user wants to decode
+    for key in range(0, 26):
+        EOut = ""
+    # for each number from 0 to 26 it will do set EOut equal to an empty string, allowing the rest to add to EOut without itjust adding to the same string and making it actually reset before adding the next loop
+        for x in f.lower():
+            if x not in alphabet:
+                EOut += x
+        # for each character in the file it checks if its not in alphabet, if it isn't it just re-adds the character to EOut
+            else:
+                if (alphabet.find(x) - key) > len(alphabet):
+                    g = (int(alphabet.find(x)) + 26) - key
+                    l = alphabet[g]
+                    EOut += l
+                # for each character in text it checks if the math would go over the length of the alphabet, if it does it does more math and finds the correct character in the alphabet to change it to based on the key then adds it to EOut
+                else:
+                    o = alphabet.find(x) - key
+                    k = alphabet[o]
+                    EOut += k
+        print(f" {key}) {EOut}")
+    file.close()
+    # If the math does not exceed the length of the alphabet then it just finds the correct character to change it to and adds it to EOut without doing that more math, and then closes the file
 
 # main method declaration
 def main():
@@ -65,7 +117,9 @@ def main():
               f"[1]: Encode a custom message.\n"
               f"[2]: Encode file.\n"
               f"[3]: Decode file.\n"
-              f"[4]: Exit.")
+              f"[4]: Decode file with an unknown key. \n"
+              f"[5]: Exit.")
+    # menu used to asking the user what they want to do.
 
         selection = input("Choose an option:")
 
@@ -76,8 +130,11 @@ def main():
         elif selection == "3":
             decode_file()
         elif selection == "4":
+            decode_unknown_key()
+        elif selection == "5":
             print("Goodbye.")
             exit()
+        # calls the correct function depending on what the user chooses to do in the menu
         else:
             print("Invalid choice. Please try again.")
 
